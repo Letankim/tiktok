192f466b3248bb23a1a15e68c329836cb3492014
Letankim <89767092+Letankim@users.noreply.github.com>
2023-04-27 16:03:19 +0700

    cấu hình Prettier

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,6 @@
 import myAlert from "~/myAlert"
 function Button() {
-    return (<button onClick={e=> myAlert()}>Click me!</button>)
+    return (<button onClick={e => myAlert()}>Click me!</button>)
 }
 
 export default Button
